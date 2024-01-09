a0135f7dd6a42c5fd0462f94fa6e953455633838
nishant-dash <nishant.dash@canonical.com>
2024-01-09 19:11:48 +0400

    cleaned ip project files

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
 """Manage package and distribution."""
-import subprocess
-from typing import List
-
 from setuptools import setup
 
-
-def find_version() -> str:
-    """Parse rtab version based on the git tag.
-
-    :return: Version of the package.
-    :rtype: str
-    """
-    try:
-        cmd: List[str] = ["git", "describe", "--tags", "--always", "HEAD"]
-        gitversion: str = subprocess.check_output(cmd, stderr=subprocess.DEVNULL).decode().strip()
-        if all(char.isdigit() or char == "." for char in gitversion):
-            return gitversion
-        build: List[str] = gitversion.split("-")
-        return f"{build[0]}.post{build[1]}"
-    except IndexError:
-        cmde: List[str] = ["git", "rev-list", "--count", "HEAD"]
-        commits_count: str = (
-            subprocess.check_output(cmde, stderr=subprocess.DEVNULL).decode().strip()
-        )
-        return f"0.1.dev{commits_count}"
-    except subprocess.CalledProcessError:
-        return "0.1.dev0"
-
-
-setup(version=find_version())
+setup()
